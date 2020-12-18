f731c48906397eb6ee14c1ea07bb4a87f14c266c
Javonte Tuppince <taetupp47@gmail.com>
2020-12-18 17:58:40 -0500

    saving all the data i have at the monmoent

--- Develop/db/cordio.js
+++ Develop/db/cordio.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CardioSchema = new Schema({
-    cardio_name: {
+    name: {
     type: String,
     trim: true,
     required: "Workout name Required"
